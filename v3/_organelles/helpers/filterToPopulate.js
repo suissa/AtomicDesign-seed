1e5315024533e2f2a22d8d192c0beab9a93bbb34
Suissa <jnascimento@gmail.com>
2017-01-07 12:06:54 -0200

    v3 ajeitando-se

--- v3/_organelles/helpers/filterToPopulate.js
+++ v3/_organelles/helpers/filterToPopulate.js
@@ -1,9 +1,8 @@
 const fs = require('fs')
-
-const PATH = __filename.split('v3/')[0]+'v3/_atoms/atom-'
+const PROJECT_NAME = 'v3'
+const PATH = __filename.split(PROJECT_NAME + '/')[0] + PROJECT_NAME + '/_atoms/atom-'
 const existsFile = (file) => fs.existsSync(file)
 
-// map
 const filterToPopulate = (field, i) => {
   let atomConfig = PATH + field.trim() + '-config.js'
 
