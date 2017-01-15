9bec5ada669c54a8f62e31769740b455afb980ff
Suissa <jnascimento@gmail.com>
2017-01-15 06:52:52 -0200

    Todos modulos refatorados para pegar as routas ou locais ou do _config

--- v3/modules/Park/index.js
+++ v3/modules/Park/index.js
@@ -1,6 +1,12 @@
 const CONFIG = require('./config')
 const ROUTER_PATH = './routes/'
-const ROUTES_CONFIG = require('./routes.config.js')
+
+const fs = require('fs')
+const existsFile = (file) => fs.existsSync(file)
+const getRoutesConfig = () => existsFile('./routes.config.js') ||
+                              require('../../_config/routes/routes.config')
+                              
+const ROUTES_CONFIG = getRoutesConfig()
 const ROUTES = require('./config.module.routes')(ROUTES_CONFIG)
 
 const ROUTER = require(ROUTER_PATH + CONFIG.ROUTER)
